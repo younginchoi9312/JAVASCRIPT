d42d1bf818b286a8f1ed08d78b4128ada7765909
younginchoi9312 <78770261+younginchoi9312@users.noreply.github.com>
2021-03-04 17:53:07 +0900

    Add files via upload

---
+++ lecture05.js
@@ -0,0 +1,16 @@
+// var a = 100, b = 3.14;
+// var c = "안녕하세요", d = "a";
+// var e = true, f = false;
+
+// console.log(a, typeof(a))
+// console.log(c, typeof(c))
+// console.log(e, typeof(e))
+
+var n = 1;
+console.log(n, typeof(n))
+
+var b = true;
+console.log(b, typeof(b))
+
+var s = "문자열입니다."
+console.log(s, typeof(s))
